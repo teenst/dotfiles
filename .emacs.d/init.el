4f4f683d5e445f638e47d8ae949d687a6357f4f3
teenst <teens.twenties@gmail.com>
2013-12-13 23:16:50 +0900

    add autocomplete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -140,16 +140,20 @@
 
 
 ;; auto-complete
-;; M-x package-install RET auto-complete
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories 
-             "~/.emacs.d/elpa/auto-complete-20130724.1750/dict/")
-(ac-config-default)
-;; (setq ac-auto-start nil)
-;; (ac-set-trigger-key "TAB")
-(setq ac-use-menu-map t)
-(define-key ac-menu-map "\C-n" 'ac-next)
-(define-key ac-menu-map "\C-p" 'ac-previous)
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories 
+               "~/.emacs.d/elpa/auto-complete-20131128.233/dict/")
+  (ac-config-default)
+  (setq ac-use-menu-map t)
+  (setq ac-auto-start 2)  ;; n文字以上の単語の時に補完を開始
+  (setq ac-delay 0.05)  ;; n秒後に補完開始
+  (setq ac-use-fuzzy t)  ;; 曖昧マッチ有効
+  (setq ac-use-comphist t)  ;; 補完推測機能有効
+  (setq ac-auto-show-menu 0.05)  ;; n秒後に補完メニューを表示
+  (setq ac-quick-help-delay 0.5)  ;; n秒後にクイックヘルプを表示
+  (setq ac-ignore-case nil)  ;; 大文字・小文字を区別する
+  (define-key ac-menu-map "\C-n" 'ac-next)
+  (define-key ac-menu-map "\C-p" 'ac-previous))
 
 
 ;; Helm-mode
